dbbb221fafb7e867dfb4973eb50529074dc66f2f
Favour Taiwo <66367148+Izzy4999@users.noreply.github.com>
2023-06-15 22:42:54 +0100

    Update genreServices.js

--- src/services/genreServices.js
+++ src/services/genreServices.js
@@ -3,3 +3,7 @@ import http from "./httpServices";
 export function getGenres() {
   return http.get("/api/genres");
 }
+
+export function getGenres() {
+     return http.post("/api/genres", movie);
+}
