0cc38e80c87b0a75fec4ceb6e5a83688f03ffa6b
Favour Taiwo <66367148+Izzy4999@users.noreply.github.com>
2023-06-15 22:30:04 +0100

    Update userServices.js

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,6 +1,6 @@
 import http from "./httpServices";
 
-const apiEndpoint = "/users/register";
+const apiEndpoint = "/api/users/register";
 
 export function register(email, password, name) {
   return http.post(apiEndpoint, { email, password, name });
